a83abb8d98cc2633026eb020d654449d965b278b
Briana Vecchione <briana.vecchione@gmail.com>
2017-02-26 14:51:14 -0500

    Fix Bugs in Production
    Changes:
    - clean up homeCtrl, edit gulp error msg to print stack trace instead of failing instantly
    - Fix mobile on network: when ctrlCtrl was converted to have seperate scope from homeCtrl, variables were no longer seen in networkCtrl. For now, they're set in homeCtrl manually. Long term, the network should be isolated from homeCtrl scope and create a service for network that would provide values as well as give other directives a way to communicate directly to it ithout sending global messages to the system.
    - Fix navigation on map: ended up being a broken link between controls controller and homeCtrl that was removed when controls were isolated as a separate directive.
    - Add global util service to fix mobile graph. Mobile flags in homeCtrl had to be exposed to the directive, so they were converted to global util service.

--- js/edit-entity/service/entityService.js
+++ js/edit-entity/service/entityService.js
@@ -7,17 +7,19 @@
     'use strict';
 
     var entityServiceDependencies = [
+        '$http',
         'fundingConnectionService',
         'connectionService',
         'financeService',
         'locationService',
         'categoryService',
+        'config',
+        '$q',
         EntityService
     ];
 
-    function EntityService(fundingConnectionService, connectionService, financeService, locationService,
-                           categoryService
-    ) {
+    function EntityService($http, fundingConnectionService, connectionService, financeService, locationService,
+                           categoryService, config, $q) {
 
         var entityTypes = {
             'Government': true,
@@ -37,16 +39,16 @@
         }
 
         function loopAndInit(modelArray, initModelFunction) {
-            if( !( isDef(modelArray) || angular.isArray(modelArray) ) ) return [initModelFunction()];
+            if (!( isDef(modelArray) || angular.isArray(modelArray) )) return [initModelFunction()];
             var arrayIndex,
                 arrayValue,
                 newModelArray = [];
 
-            for ( arrayIndex in modelArray ) {
-                if(!modelArray.hasOwnProperty(arrayIndex)) continue;
+            for (arrayIndex in modelArray) {
+                if (!modelArray.hasOwnProperty(arrayIndex)) continue;
                 arrayValue = modelArray[arrayIndex];
 
-                if(!angular.isObject(arrayValue)) continue;
+                if (!angular.isObject(arrayValue)) continue;
 
                 newModelArray.push(initModelFunction(arrayValue));
             }
@@ -85,7 +87,7 @@
             // this.relations = loopAndInit(defObj.relations, connectionService.getConnectionModel);
 
 
-            this.generateDBModel = function() {
+            this.generateDBModel = function () {
                 var dbModel = new Entity(self);
                 dbModel.locations.pop();
                 dbModel.locations.pop();
@@ -121,18 +123,79 @@
 
         }
 
-        this.getEntityModel = function (obj) {
+        function getEntityModel(obj) {
             return new Entity(obj);
-        };
+        }
 
-        this.getEntityTypes = function (){
+        function getEntityTypes() {
             return entityTypes;
-        };
+        }
 
-        this.getInfluenceTypes = function () {
+        function getInfluenceTypes() {
             return influenceTypes;
-        };
+        }
 
+        function getFromAPI() {
+            return $http.get(config.apiHost + 'api/entities');
+            // var defer = $q.defer();
+            // defer.resolve({
+            //     "nodes": [
+            //         {
+            //             "categories": [
+            //                 {"id": 3, "name": "General Civic Tech"},
+            //                 {"id": 4, "name": "Social Services"},
+            //                 {"id": 5, "name": "Jobs & Education"},
+            //                 {"id": 6, "name": "GovTech"}
+            //             ],
+            //             "collaborations": [
+            //                 {"details": null, "entity": "Alexander Howard", "entity_id": 290, "id": 911}
+            //             ],
+            //             "data_given": [],
+            //             "data_received": [],
+            //             "description": "I create experiences / services that solve public problems. Also - Adjunct Professor at @NYU_Wagner",
+            //             "employees": null,
+            //             "employments": [],
+            //             "expenses": [],
+            //             "followers": 1655,
+            //             "grants_given": [],
+            //             "grants_received": [],
+            //             "id": 0,
+            //             "influence": "Local",
+            //             "investments_made": [],
+            //             "investments_received": [],
+            //             "key_people": [],
+            //             "locations": [
+            //                 {
+            //                     "address_line": null,
+            //                     "coordinates": [40.782, -73.8317],
+            //                     "country": "United States",
+            //                     "country_code": "US",
+            //                     "district": "NY",
+            //                     "full_address": "New York, NY",
+            //                     "id": 0,
+            //                     "locality": "New York",
+            //                     "postal_code": null
+            //                 }
+            //             ],
+            //             "name": "Yasmin Fodil",
+            //             "nickname": "Yasmin",
+            //             "relations": [],
+            //             "revenues": [],
+            //             "twitter_handle": "@yasminfodil",
+            //             "type": "Individual",
+            //             "url": "http://yasminfodil.com"
+            //         }
+            //     ]
+            // });
+            // return defer.promise;
+        }
+
+        return {
+            "getEntityModel": getEntityModel,
+            "getEntityTypes": getEntityTypes,
+            "getInfluenceTypes": getInfluenceTypes,
+            "getFromAPI": getFromAPI
+        };
     }
 
     angular.module('civic-graph')
