12f671d51767b56d67d4ea9efa60bddcdd3a9b55
prathmj <pjuneja@nd.edu>
2018-06-14 16:47:19 -0400

    Not a major commit. We're having issues right now with the mobile details pop up covering parts of the map. Temporary solution is to move the pop up all the way to the top of the screen. Anything further will be part of a larger reworking of the way mobile rendering happens.

--- js/main/homeCtrl.js
+++ js/main/homeCtrl.js
@@ -206,9 +206,8 @@
         });
 
         $scope.animationsEnabled = true;
-		
+		//THIS IS WHERE THE MOBILE STUFF GOES
 		$scope.toggleNode = function (type) {
-			console.log("Inside the controlCtrl toggleNode");
             $scope.$broadcast('toggleNode', {'name': type, 'enabled': $scope.entityTypes[type]
             });
         };
@@ -216,6 +215,10 @@
             $scope.$broadcast('toggleLink', {'name': type, 'enabled': $scope.connectionTypes[type]
             });
         };
+        $scope.changeSizeBy = function () {
+            $scope.$broadcast('changeSizeBy', $scope.sizeBy);
+        };
+
     }
 
     angular.module('civic-graph')
