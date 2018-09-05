25d3af578351799e30acebb5611cf1cbb6344fc8
seerocode <ro@DESKTOP-GIDKRUJ.localdomain>
2018-09-05 10:42:27 -0400

    rolled back temp fix for power bi dashboard as Azure is accepting changes now

--- js/analytic/analyticsDirective.js
+++ js/analytic/analyticsDirective.js
@@ -12,7 +12,6 @@
     function analyticsDirective(){
         return {
             templateUrl: 'js/analytic/analytics.html',
-            // templateUrl: 'https://msit.powerbi.com/view?r=eyJrIjoiNDRmNzc4NjYtMWM2OS00NGFkLWI1MDctY2FhY2IwNzcyN2I5IiwidCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsImMiOjV9',
             restrict   : 'E',
             scope      : {},
             controller   : 'analyticsCtrl'
